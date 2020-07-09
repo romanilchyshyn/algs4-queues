66ec6f90be33e776779270482a3e932f1350d00d
Roman Ilchyshyn <romanilchyshyn@gmail.com>
2020-07-09 20:47:54 +0300

    Fix Permutation client.

--- Permutation.java
+++ Permutation.java
@@ -6,21 +6,16 @@
 
 import edu.princeton.cs.algs4.StdIn;
 
-import java.util.NoSuchElementException;
-
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
 
         RandomizedQueue<String> rq = new RandomizedQueue<String>();
 
-        while (true) {
-            try {
-                String s = StdIn.readString();
-                rq.enqueue(s);
-            } catch (NoSuchElementException e) {
-                break;
-            }
+
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            rq.enqueue(s);
         }
 
         for (String e : rq) {
